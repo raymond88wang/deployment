65798c879d59f4f9b935d9d861cb8e794c5bb1b9
raymond88wang <raymond88wang@gmail.com>
2023-09-12 23:17:35 -0700

    sdfdsf

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express()
 mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}, 
   () => { 
     console.log('connected to mongo: ', process.env.MONGO_URI)
-    console.log(__dirname + '/views')
+    console.log(process.cwd() + '/views')
   }
 )
 
