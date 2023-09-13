d05ee4669f7da16c8d5bfbee4bd07fb76584c319
raymond88wang <raymond88wang@gmail.com>
2023-09-12 23:08:03 -0700

    Sdgdsg

--- server.js
+++ server.js
@@ -15,7 +15,8 @@ mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopolo
 app.use(methodOverride('_method'))
 app.use(express.urlencoded({extended: true}))
 app.use(express.static('public'))
-app.set('views', process.cwd() + '/views')
+console.log(__dirname + '/views')
+app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 
