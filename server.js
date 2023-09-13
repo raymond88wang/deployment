ff4e8bb35b5485d1b2de53890d18a98162e02615
raymond88wang <raymond88wang@gmail.com>
2023-09-12 23:16:10 -0700

    sdgfsdg

--- server.js
+++ server.js
@@ -8,17 +8,19 @@ require('dotenv').config()
 const PORT = process.env.PORT
 const app = express()
 mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}, 
-  () => { console.log('connected to mongo: ', process.env.MONGO_URI) }
+  () => { 
+    console.log('connected to mongo: ', process.env.MONGO_URI)
+    console.log(__dirname + '/views')
+  }
 )
 
 // MIDDLEWARE
 app.use(methodOverride('_method'))
 app.use(express.urlencoded({extended: true}))
 app.use(express.static('public'))
-console.log(__dirname + '/views')
 app.set('views', __dirname + '/views')
-app.engine('jsx', require('express-react-views').createEngine())
 app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
 
 // ROUTES
 app.get('/', (req, res) => {
